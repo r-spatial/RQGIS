a76bda9f21aba14d2fc02c1c9351d4478e32fe4a
pat-s <patrick.schratz@gmail.com>
2016-11-22 12:03:18 +0100

    add unit test for build_cmds()

---
+++ tests/testthat/test-build-cmds.R
@@ -0,0 +1,13 @@
+context("build_cmds")
+
+test_that("correct output of build_cmds", {
+  skip_on_cran()
+  out <- build_cmds()
+  
+  # check that 'cmd' and 'py_cmd' are build
+  expect_equal(length(out), 2)
+  # check that all lines of 'cmd' are build
+  expect_equal(length(out$cmd), 4)
+  # check that all lines of 'py_cmd' are build
+  expect_equal(length(out$py_cmd), 14)
+})
