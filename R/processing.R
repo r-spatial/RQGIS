497cefc514700b534ecda3733075a4a1e9428e5e
jannes-m <malnamalja@gmx.de>
2016-12-16 11:50:45 +0100

    making run_qgis work again, by accident processing.runalg was deleted...

--- R/processing.R
+++ R/processing.R
@@ -853,6 +853,34 @@ run_qgis <- function(alg = NULL, params = NULL, check_params = TRUE,
       paste(c(ext@xmin, ext@xmax, ext@ymin, ext@ymax), collapse = ",")
   }
   
+  # run QGIS
+  nm <- names(params)
+  val <- as.character(unlist(params))
+  # shellquote algorithm name
+  start <- shQuote(alg)
+  # True, False and None should not be put among parentheses!!
+  ind <- !grepl("True|False|None", val)
+  # shellquote paths and numeric input (the latter is not necessary but
+  # doesn't harm either)
+  val[ind] <- shQuote(val[ind])
+  # build the Python command
+  args <- paste(val, collapse = ", ")
+  args <- paste0(paste(start, args, sep = ", "))
+  # run QGIS command (while catching possible error messages)
+  msg <- execute_cmds(processing_name = "processing.runalg",
+                      params = args,
+                      qgis_env = qgis_env,
+                      intern = ifelse(Sys.info()["sysname"] == "Darwin",
+                                      FALSE, TRUE))
+  if (any(grepl("Error", msg))) {
+    stop(msg)
+  }
+  # if a message was produce show it in the console
+  if (show_msg & length(msg) > 0) {
+    message(msg)
+  }
+  
+  
   # load output
   if (!is.null(load_output)) {
     ls_1 <- lapply(load_output, function(x) {
