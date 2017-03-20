e61ad872fb854895c4bc2a159ffa622627eddeb4
jannes-m <malnamalja@gmx.de>
2017-03-21 00:14:20 +0100

    rewriting qgis_session_info

--- R/processing.R
+++ R/processing.R
@@ -123,139 +123,27 @@ set_env <- function(root = NULL, ltr = TRUE) {
 #'  \item{supported_saga_versions: character vector representing the SAGA
 #'  versions supported by the QGIS installation.}
 #' }
-
 #' @author Jannes Muenchow, Victor Olaya, QGIS core team
+#' @importFrom reticulate py_to_r py_run_file py_run_string
 #' @export
 #' @examples 
 #' \dontrun{
 #' qgis_session_info()
 #' }
-qgis_session_info <- function(qgis_env = set_env()) {
-  
-  # set the paths
-  cwd <- getwd()
-  on.exit(setwd(cwd))
-  tmp_dir <- tempdir()
-  setwd(tmp_dir)
-  
-  # build the raw scripts
-  cmds <- build_cmds(qgis_env)
-  
-  # extend the python command
-  py_cmd <- 
-    c(cmds$py_cmd,
-      "import csv",
-      "import re",
-      paste0("from processing.algs.saga.SagaAlgorithmProvider",
-             " import SagaAlgorithmProvider"),
-      "from processing.algs.saga import SagaUtils",
-      "from processing.algs.grass.GrassUtils import GrassUtils",
-      "from processing.algs.grass7.Grass7Utils import Grass7Utils",
-      "from processing.algs.otb.OTBAlgorithmProvider import OTBAlgorithmProvider",
-      "from processing.algs.otb.OTBUtils import getInstalledVersion",
-      "from processing.algs.taudem.TauDEMUtils import TauDEMUtils",
-      "from osgeo import gdal",
-      "from processing.tools.system import isWindows, isMac",
-      # QGIS version
-      "qgis = QGis.QGIS_VERSION",
-      # GRASS versions
-      # grassPath returns "" if called under Linux and if there is no GRASS 
-      # installation
-      "GrassUtils.checkGrassIsInstalled()",
-      "g6 = GrassUtils.isGrassInstalled",
-      "if g6 is True and isWindows():",
-      "  g6 = GrassUtils.grassPath()",
-      "  g6 = re.findall('grass-.*', g6)",
-      "if g6 is True and isMac:",
-      "  g6 = GrassUtils.grassPath()",
-      "  g6 = os.listdir(g6)",
-      "  delim = ';'",
-      "  g6 = delim.join(g6)",
-      "  g6 = re.findall(';(grass[0-9].);', g6)",
-      "Grass7Utils.checkGrass7IsInstalled()",
-      "g7 = Grass7Utils.isGrass7Installed",
-      "if g7 is True and isWindows():",
-      "  g7 = Grass7Utils.grassPath()",
-      "  g7 = re.findall('grass-.*', g7)",
-      "if g7 is True and isMac:",
-      "  g7 = Grass7Utils.grassPath()",
-      "  g7 = os.listdir(g7)",
-      "  delim = ';'",
-      "  g7 = delim.join(g7)",
-      "  g7 = re.findall(';(grass[0-9].);', g7)",
-      # installed SAGA version usable with QGIS
-      "saga = SagaUtils.getSagaInstalledVersion()",
-      # supported SAGA versions
-      "my_dict = SagaAlgorithmProvider.supportedVersions",
-      "saga_versions = my_dict.keys()",
-      "saga_versions.sort()",
-      
-      # this is good to have for the future, but so far, I would not report 
-      # these software versions since we don't know if they actually work
-      # with QGIS (without additional functions such as run_taudem...)
-      # OTB versions
-      # "otb = getInstalledVersion()",
-      #"otb = OTBUtils.getInstalledVersion()",
-      
-      # GDAL
-      # "gdal = gdal.VersionInfo('VERSION_NUM')",
-      # "gdal = '.'.join([gdal[0], gdal[2], gdal[4]])",
-      
-      # write list for 'out.csv'
-      "ls = [qgis, g6, g7, saga, saga_versions]",
-      # "ls = [qgis, g6, g7, saga, saga_versions, otb, gdal]",
-      ### TauDEM versions (currently not in use because no function to extract
-      ### Taudem version in 'TauDEMUtils')
-      # "TauDEMUtils.taudemMultifilePath()",
-      
-      paste0("with open('", tmp_dir, "/out.csv', 'w') as f:"),
-      "  writer = csv.writer(f)",
-      "  for item in ls:",
-      "    writer.writerow([unicode(item).encode('utf-8')])",
-      "f.close()",
-      "")
-  
-  py_cmd <- paste(py_cmd, collapse = "\n")
-  # harmonize slashes
-  py_cmd <- gsub("\\\\", "/", py_cmd)
-  py_cmd <- gsub("//", "/", py_cmd)
-  # save the Python script
-  cat(py_cmd, file = "py_cmd.py")
-  
-  # build the batch/shell command to run the Python script
-  if (Sys.info()["sysname"] == "Windows") {
-    cmd <- c(cmds$cmd, "python py_cmd.py")
-    # filename
-    f_name <- "batch_cmd.cmd"
-    batch_call <- f_name
-  } else {
-    cmd <- c(cmds$cmd, "/usr/bin/python py_cmd.py")
-    # filename
-    f_name <- "batch_cmd.sh"
-    batch_call <- "sh batch_cmd.sh"
+qgis_session_info <- function() {
+  tmp <- try(expr =  reticulate::py_run_string("app")$app,
+             silent = TRUE)
+  if (inherits(tmp, "try-error")) {
+    open_app()
   }
-  # put each element on its own line
-  cmd <- paste(cmd, collapse = "\n")
-  # save the batch file to the temporary location
-  cat(cmd, file = f_name)
-  # run Python via the command line
-  system(batch_call, intern = TRUE)
-  
+  py_file <- system.file("python", "qgis_session_info.py", package = "RQGIS")
+  out <- py_run_file(py_file)
+
   # retrieve the output
-  out <- utils::read.csv(file.path(tempdir(), "out.csv"), header = FALSE)  
-  out$V1 <- gsub("False", FALSE, out$V1)
-  out$V1 <- gsub("True", TRUE, out$V1)
-  out <- as.list(gsub("\\[|\\]|u'|'", "", out$V1))
-  out[[5]] <- unlist(strsplit(out[[5]], split = ", "))
+  out <- out$ls
   names(out) <- c("qgis_version", "grass6", "grass7", "saga",
                   "supported_saga_versions")
   
-  # names(out) <- c("qgis_version", "grass6", "grass7", "saga",
-  #                 "supported_saga_versions", "orfeo_toolbox",
-  #                 "GDAL")
-  # clean up after yourself
-  # unlink(file.path(tmp_dir, "out.csv"))
-  # return the output
   out
 }
 
