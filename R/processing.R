0e22c15ae310d084a5d1cb2725293244f87d0dce
jannes-m <malnamalja@gmx.de>
2017-03-28 09:19:43 +0200

    using reticulate with run_qgis

--- R/processing.R
+++ R/processing.R
@@ -513,6 +513,7 @@ get_args_man <- function(alg = "", options = FALSE,
 #' @importFrom sp SpatialPointsDataFrame SpatialPolygonsDataFrame
 #' @importFrom sp SpatialLinesDataFrame
 #' @importFrom raster raster
+#' @importFrom reticulate py_run_string
 #' @examples
 #' \dontrun{
 #' # set the environment
@@ -655,12 +656,7 @@ run_qgis <- function(alg = NULL, params = NULL, check_params = TRUE,
   
   # shellquote algorithm name
   start <- shQuote(alg)
-  # retrieve specified function arguments, i.e. the values
-  # Sometimes function arguments are already shellquoted. Shellquoting them 
-  # again will result in an error, e.g., grass7:r.viewshed
-  # Hence, get rid off shellQuotes (if there are any) before you shellQuote
-  # again... and ShellQuotes (or at least quotes) are needed when using the
-  # command line 
+  # build the Python command
   val <- vapply(params, function(x) {
     # get rid off shellQuotes 
     tmp <- unlist(strsplit(as.character(x), ""))
@@ -671,24 +667,14 @@ run_qgis <- function(alg = NULL, params = NULL, check_params = TRUE,
     ifelse(grepl("True|False|None", tmp), tmp, shQuote(tmp))
   }, character(1))
   
-  # build the Python command
+
   args <- paste(val, collapse = ", ")
   args <- paste0(paste(start, args, sep = ", "))
-  # run QGIS command (while catching possible error messages)
-  msg <- execute_cmds(processing_name = "processing.runalg",
-                      params = args,
-                      qgis_env = qgis_env,
-                      intern = ifelse(Sys.info()["sysname"] == "Darwin",
-                                      FALSE, TRUE))
-  if (any(grepl("Error", msg))) {
-    stop(msg)
-  }
-  # if a message was produced, show it in the console
-  if (show_msg && length(msg) > 0 && !identical(msg, tempdir())) {
-    message(msg)
-  }
+  cmd <- paste0("processing.runalg(", args, ")")
   
-  
+  # run QGIS
+  py_run_string(cmd)
+
   # load output
   if (!is.null(load_output)) {
     ls_1 <- lapply(load_output, function(x) {
