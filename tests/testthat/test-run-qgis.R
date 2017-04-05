9ed11cce9e4ac8cc134d119c231881004126b17b
pat-s <patrick.schratz@gmail.com>
2017-04-05 22:54:00 +0200

    line-by line testing of open_app()

--- tests/testthat/test-run-qgis.R
+++ tests/testthat/test-run-qgis.R
@@ -12,9 +12,111 @@ context("run_qgis")
 # qgis_env <- set_env("C:/OSGeo4W64/", ltr = FALSE)  
 
 
-open_app()
+##### manual testing for travis
 
-as.character(py_run_string("with Capturing() as output_alglist:\n  processing.alglist()")$output_alglist)
+qgis_env <- set_env()
+
+settings <- as.list(Sys.getenv())
+# since we are adding quite a few new environment variables these will remain
+# (PYTHONPATH, QT_PLUGIN_PATH, etc.). We could unset these before exiting the
+# function but I am not sure if this is necessary
+
+# Well, well, not sure if we should change it back or at least we have to get
+# rid off Anaconda Python
+# on.exit(do.call(Sys.setenv, settings))
+
+if (Sys.info()["sysname"] == "Windows") {
+  # run Windows setup
+  setup_win(qgis_env = qgis_env)
+} else if (Sys.info()["sysname"] == "Linux") {
+  # append pythonpath to import qgis.core etc. packages
+  python_path <- Sys.getenv("PYTHONPATH")
+  qgis_python_path <- paste0(qgis_env$root, "/share/qgis/python")
+  if (python_path != "" & !grepl(qgis_python_path, python_path)) {
+    qgis_python_path <- paste(qgis_python_path, Sys.getenv("PYTHONPATH"), 
+                              sep=":")
+  } 
+} else if (Sys.info()["sysname"] == "Darwin") { 
+  python_path <- Sys.getenv("PYTHONPATH")
+  # PYTHONPATH only applies to homebrew installation - todo: account for Kyngchaos 
+  qgis_python_path <- paste0(qgis_env$root, "/Contents/Resources/python/:/usr/local/lib/qt-4/python2.7/site-packages:/usr/local/lib/python2.7/site-packages:$PYTHONPATH")
+  if (python_path != "" & !grepl(qgis_python_path, python_path)) {
+    qgis_python_path <- paste(qgis_python_path, Sys.getenv("PYTHONPATH"), 
+                              sep=":")    
+  }
+  
+  Sys.setenv(QGIS_PREFIX_PATH = paste0(qgis_env$root, "/Contents/MacOS/")) ### is this not needed on Windows/Linux? Without `open_app() does not work on Mac`
+} 
+Sys.setenv(PYTHONPATH = qgis_python_path)
+
+
+# define path where QGIS libraries reside to search path of the
+# dynamic linker
+ld_library <- Sys.getenv("LD_LIBRARY_PATH")
+
+if (!Sys.info()["sysname"] == "Darwin") {
+  qgis_ld <- paste(paste0(qgis_env$root, "/lib"))
+} else {
+  qgis_ld <- paste(paste0(qgis_env$qgis_prefix_path, "/MacOS/lib/:/Applications/QGIS.app/Contents/Frameworks/")) # homebrew
+}
+if (ld_library != "" & !grepl(paste0(qgis_ld, ":"), ld_library)) {
+  qgis_ld <- paste(paste0(qgis_env$root, "/lib"),
+                   Sys.getenv("LD_LIBRARY_PATH"), sep=":")
+}
+Sys.setenv(LD_LIBRARY_PATH = qgis_ld)
+
+# not sure, if we need the subsequent test for Linux & Mac since the Python 
+# binary should be alway /usr/bin/python.exe -> ask Patrick Patrick: you are right, python binary is always in /usr/bin for Linux & Mac
+if (Sys.info()["sysname"] == "Windows") {
+  # compare py_config path with set_env path!!
+  a <- py_config()
+  py_path <- gsub("\\\\bin.*", "", normalizePath(a$python))
+  if (!identical(py_path, qgis_env$root)) {
+    stop("Wrong Python binary. Restart R and check!")
+  }
+}
+
+# suppress messages for homebrew Mac installation
+if (Sys.info()["sysname"] == "Darwin") {
+  Sys.setenv(QGIS_DEBUG=-1)
+}
+
+
+# make sure that QGIS is not already running (this would crash R)
+# app = QgsApplication([], True)  # see below
+tmp <- try(expr =  py_run_string("app")$app,
+           silent = TRUE)
+if (!inherits(tmp, "try-error")) {
+  stop("Python QGIS application is already running.")
+}
+
+py_run_string("import os, sys, re, webbrowser")
+py_run_string("from qgis.core import *")
+py_run_string("from osgeo import ogr")
+py_run_string("from PyQt4.QtCore import *")
+py_run_string("from PyQt4.QtGui import *")
+py_run_string("from qgis.gui import *")
+set_prefix <- paste0("QgsApplication.setPrefixPath(r'",
+                     qgis_env$qgis_prefix_path, "', True)")
+code <- paste0("sys.path.append(r'", qgis_env$python_plugins, "')")
+py_run_string(code)
+py_run_string(set_prefix)
+py_run_string("app = QgsApplication([], True)")
+py_run_string("QgsApplication.initQgis()")
+# attach further modules 
+py_file <- system.file("python", "import_setup.py", package = "RQGIS")
+py_run_file(py_file)
+# attach Barry's capture and our RQGIS class (needed for alglist, algoptions,
+# alghelp)
+py_file <- system.file("python", "helper_funs.py", package = "RQGIS")
+py_run_file(py_file)
+# initialize our RQGIS class
+py_run_string("RQGIS = RQGIS()")
+
+
+# open_app()
+
+# as.character(py_run_string("with Capturing() as output_alglist:\n  processing.alglist()")$output_alglist)
 
 
 test_that("Test, if QGIS-algorithms are working?", {
