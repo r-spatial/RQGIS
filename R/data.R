70322517c9e0a1462c316f7d8bb7922b23c62a52
pat-s <patrick.schratz@gmail.com>
2017-04-06 23:45:46 +0200

    update /data

--- R/data.R
+++ R/data.R
@@ -1,5 +1,6 @@
 #' @title Random points.
 #' 
+#' @name random_points
 #' @description A [sp::SpatialPointsDataFrame()] (EPSG:32717) object
 #'   with 100 randomly sampled points (stratified by altitude). For more
 #'   details, please refer to Muenchow et al. (2013).
@@ -20,9 +21,10 @@
 #' Predictive mapping of species richness and plant species' distributions of a
 #' Peruvian fog oasis along an altitudinal gradient. Biotropica 45, 5, 557-566,
 #' doi: 10.1111/btp.12049.
-"random_points"
+NULL
 
 #' @title Digital elevation model (DEM) of the Mongón study area.
+#' @name dem
 #'
 #' @description A [raster::raster()] object (EPSG:32717) representing
 #'   altitude (ASTER GDEM, LP DAAC 2012).  For more details, please refer to
@@ -43,9 +45,10 @@
 #' at the U.S. Geological Survey (USGS) Earth Resources Observation
 #' and Science (EROS) Center. Available at: https://lpdaac.usgs.gov/
 #' (last accessed 25 January 2012).
-"dem"
+NULL
 
 #' @title Normalized difference vegetation index for the Mongón study area.
+#' @name ndvi
 #'
 #' @description NDVI [raster::raster()] (EPSG:32717) computed from a
 #'   Landsat scene (path 9, row 67, acquisition date: 09/22/2000; USGS 2013).
@@ -64,4 +67,4 @@
 #'   
 #' USGS (2013): U.S. Geological Survey. Earth Explorer. Available at: 
 #' http://earthexplorer.usgs.gov/ (last accessed 1 March 2013).
-"ndvi"
+NULL
