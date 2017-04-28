d8e12334989cc54c00b698da158898431ec89799
pat-s <patrick.schratz@gmail.com>
2017-04-28 13:50:35 +0200

    add get_usage test

---
+++ tests/testthat/test-get-usage.R
@@ -0,0 +1,12 @@
+library("RQGIS")
+library("testthat")
+
+context("get_usage")
+
+test_that("get_usage() yields correct output", {
+  
+  expect_output(get_usage(alg = "qgis:polygoncentroids", intern = FALSE))
+  
+  expect_is(get_usage(alg = "qgis:polygoncentroids", intern = TRUE), "character")
+  
+})
