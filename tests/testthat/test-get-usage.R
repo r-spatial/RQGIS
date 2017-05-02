70835a436ee60b3e387ee4a40da10348813cd2c3
pat-s <patrick.schratz@gmail.com>
2017-05-02 22:56:41 +0200

    update tests

--- tests/testthat/test-get-usage.R
+++ tests/testthat/test-get-usage.R
@@ -5,6 +5,8 @@ context("get_usage")
 
 test_that("get_usage() yields correct output", {
   
+  testthat::skip_on_appveyor()
+  
   expect_output(get_usage(alg = "qgis:polygoncentroids", intern = FALSE))
   
   expect_is(get_usage(alg = "qgis:polygoncentroids", intern = TRUE), "character")
