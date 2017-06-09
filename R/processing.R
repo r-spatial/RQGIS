dfd482e2596a5f5c4b63910de18d3612e9c27274
jannes-m <malnamalja@gmx.de>
2017-06-09 10:53:38 +0200

    if the run_qgis message is empty do not print a space

--- R/processing.R
+++ R/processing.R
@@ -977,14 +977,14 @@ run_qgis <- function(alg = NULL, ..., params = NULL, load_output = FALSE,
   # If QGIS produces an error message, stop and report it
   if (grepl("Unable to execute algorithm|Error", msg)) {
     stop(msg)
-  }
+  } 
   # res contains all the output paths of the files created by QGIS
   res <- py_run_string("res")$res
   # show the output files to the user
   if (show_output_paths) {
     print(res)  
   }
-  # if there is a message, show it (if msg = "", nothing will be shown)
+  # if there is a message, show it
   if (msg != "") {
     message(msg)  
   }
