f5316d2a4c120bb9bb320d55855cc6bcf3e3039a
pat-s <patrick.schratz@gmail.com>
2017-06-12 13:35:59 +0200

    fix qgis_session_info test

--- tests/testthat/test-qgis-prerun.R
+++ tests/testthat/test-qgis-prerun.R
@@ -2,31 +2,26 @@ context("prerun")
 
 test_that("qgis_session_info yields a list as output", {
   
-  testthat::skip_on_appveyor()
-  # testthat::skip_on_travis()
-  testthat::skip_on_cran()
-  # skip_if_no_python()
-  # skip_if_no_python can be found under: 
-  # rstudio/reticulate/blob/master/tests/testthat/utils.R
+  skip_on_appveyor()
+  skip_on_cran()
   
   info <- qgis_session_info()
-  # check if the output is a list of length 5
-  expect_that(str(info), prints_text("List of 5"))
+  
+  # check if the output is a list of length 6
+  expect_length(info, 6)
   })
 
 test_that("find_algorithms finds QGIS geoalgorithms", {
   
-  testthat::skip_on_appveyor()
-  # testthat::skip_on_travis()
-  testthat::skip_on_cran()
-  # skip_if_no_python() 
+  skip_on_appveyor()
+  skip_on_cran()
 
   algs <- find_algorithms()
   # just retrieve QGIS geoalgorithms
   test <- grep("qgis:", algs, value = TRUE)
   # normally there are 101 QGIS geoalgorithms, so check if there are more than 
   # 50
-  expect_that(length(test), is_more_than(50))
+  expect_gt(length(test), 50)
   })
 
 
