bb6bebf96a8bd6712b6a71ade7b29fc871dbdbee
pat-s <patrick.schratz@gmail.com>
2017-11-27 13:19:00 +0100

    format package using r-lib/styler

--- tests/testthat/test-qgis-prerun.R
+++ tests/testthat/test-qgis-prerun.R
@@ -1,19 +1,17 @@
 context("prerun")
 
 test_that("qgis_session_info yields a list as output", {
-  
   skip_on_appveyor()
   skip_on_cran()
   # testthat::skip_on_travis()
-  
+
   info <- qgis_session_info()
-  
+
   # check if the output is a list of length 6
   expect_length(info, 6)
-  })
+})
 
 test_that("find_algorithms finds QGIS geoalgorithms", {
-  
   skip_on_appveyor()
   skip_on_cran()
   # testthat::skip_on_travis()
@@ -21,7 +19,7 @@ test_that("find_algorithms finds QGIS geoalgorithms", {
   algs <- find_algorithms()
   # just retrieve QGIS geoalgorithms
   test <- grep("qgis:", algs, value = TRUE)
-  # normally there are 101 QGIS geoalgorithms, so check if there are more than 
+  # normally there are 101 QGIS geoalgorithms, so check if there are more than
   # 50
   expect_gt(length(test), 50)
-  })
+})
