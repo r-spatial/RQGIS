a4766ce0466419885d50ca54f88a3c9f90a6887d
pat-s <patrick.schratz@gmail.com>
2018-08-13 11:26:51 +0200

    update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,10 @@
 library("testthat")
 library("RQGIS")
 
+# manually env specification for appveyor
+# not sure if this really helps!
+if(Sys.getenv("R_ARCH") == "x64") {
+  set_env("C:\\Program Files\\QGIS 2.18")
+}
+
 test_check("RQGIS")
